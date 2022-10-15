2ae3922b736f53e431c6052b5b96f917c76df0a6
cornelius mutuku <80700768+gradan-hash@users.noreply.github.com>
2022-10-15 09:14:54 +0300

    Add files via upload

---
+++ wordreplace.py
@@ -0,0 +1,11 @@
+
+
+def replace_word():
+  str = "hello I am cornelius and oo yoo "
+  word_to_replace = input("Enter word to replace: ")
+  word_replacement = input("Enter word  to replace the old word: ")
+  print(str.replace(word_to_replace ,    word_replacement))
+  
+
+replace_word()
+  
